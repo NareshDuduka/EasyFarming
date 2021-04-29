198a0de037ca9fadefad9c498a59ea84f681827b
Naresh <naresh.duduka@iiitb.org>
2021-04-29 22:07:54 +0530

    update function

--- controllers/vegetables.js
+++ controllers/vegetables.js
@@ -1,5 +1,5 @@
 const formidable = require("formidable");
-const _ = require("lodash");
+const _ = require("lodash");//for updating
 const fs = require("fs");
 const Vegetables = require("../models/vegetables");
 const { errorHandler } = require("../helpers/dbErrorHandler");
@@ -83,4 +83,54 @@ exports.remove = (req, res) => {
         });
     });
 };
-  
+
+//updating
+
+exports.update = (req, res) => {
+    
+    let form = new formidable.IncomingForm();
+    console.log("update");
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        console.log(fields.price);
+        if (err) {
+            console.log(err);
+            return res.status(400).json({
+                error: "Image could not be uploaded"
+            });
+        }
+        const {
+            item_name,
+            price,
+            quantity
+        } = fields;
+
+        if (
+            !item_name ||
+            !price ||
+            !quantity 
+        ) {
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+        let vegetables = req.vegetable;
+        vegetables= _.extend(vegetables,fields);
+
+        if (files.photo) {
+            //console.log(files.photo)
+            vegetables.photo.data = fs.readFileSync(files.photo.path);
+            vegetables.photo.contentType = files.photo.type;
+        }
+
+        vegetables.save((err, result) => {
+            if (err) {
+                console.log(err);
+                return res.staus(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        });
+    });
+};
--- routes/vegetables.js
+++ routes/vegetables.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const router=express.Router()
-const { add,vegetableById,read,remove} = require("../controllers/vegetables");
+const { add,vegetableById,read,remove,update} = require("../controllers/vegetables");
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 router.get("/vegetables/:vegetableId",read)
 router.post('/vegetables/add/:userId', requireSignin,add);
 router.delete("/vegetables/:vegetableId/:userId",requireSignin,remove)
+router.put("/vegetables/:vegetableId/:userId",requireSignin,update)
 router.param("userId",userById);
 router.param("vegetableId",vegetableById);
 
